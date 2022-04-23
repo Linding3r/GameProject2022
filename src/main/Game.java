9ad6deab294009ac15cc68ccb99fc3a9d0f62aff
Linding3r <thomasjuullindinger@yahoo.dk>
2022-04-23 12:51:31 +0200

    - added Entity.java Superclass - added Player.java changed movement function to work with booleans.

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,6 +1,9 @@
 package main;
 
 
+import entities.Player;
+
+import java.awt.*;
 
 public class Game implements Runnable {
 
@@ -8,40 +11,81 @@ public class Game implements Runnable {
   private GamePanel gamePanel;
   private Thread gameThread;
   private final int FPS_SET = 120;
+  private final int UPS_SET = 200;
+
+  private Player player;
 
   public Game() {
-      gamePanel = new GamePanel();
-      gameWindow = new GameWindow(gamePanel);
-      gamePanel.requestFocus();
-      startGameLoop();
-    }
+    initClasses();
+    gamePanel = new GamePanel(this);
+    gameWindow = new GameWindow(gamePanel);
+    gamePanel.requestFocus();
+    startGameLoop();
+  }
 
-    private void startGameLoop () {
-      gameThread = new Thread(this);
-    }
+  private void initClasses() {
+    player = new Player(200, 200);
+  }
 
-    @Override
-    public void run () {
+  private void startGameLoop() {
+    gameThread = new Thread(this);
+  }
 
-      double timePerFrame = 1000000000.0 / FPS_SET;
-      long lastFrame = System.nanoTime();
-      long now = System.nanoTime();
+  public void update() {
+    player.update();
+  }
 
-      int frames = 0;
-      long lastCheck = System.currentTimeMillis();
-      while (true) {
+  public void render(Graphics g){
+    player.render(g);
+  }
 
-        now = System.nanoTime();
-        if (now - lastFrame >= timePerFrame) {
-          gamePanel.repaint();
-          lastFrame = now;
-          frames++;
-        }
-        if (System.currentTimeMillis() - lastCheck >= 1000) {
-          lastCheck = System.currentTimeMillis();
-          System.out.println("FPS: " + frames);
-          frames = 0;
-        }
+  public Player getPlayer(){
+    return player;
+  }
+
+  @Override
+  public void run() {
+
+    double timePerFrame = 1000000000.0 / FPS_SET;
+    double timePerUpdate = 1000000000.0 / UPS_SET;
+
+    long previousTime = System.nanoTime();
+
+
+    int frames = 0;
+    int updates = 0;
+    long lastCheck = System.currentTimeMillis();
+
+    double deltaU = 0;
+    double deltaF = 0;
+
+    while (true) {
+      long currentTime = System.nanoTime();
+      deltaU += (currentTime - previousTime) / timePerUpdate;
+      deltaF += (currentTime - previousTime) / timePerFrame;
+      previousTime = currentTime;
+      if (deltaU >= 1) {
+        update();
+        updates++;
+        deltaU--;
+      }
+
+      if (deltaF >= 1) {
+        gamePanel.repaint();
+        frames++;
+        deltaF--;
+      }
+
+      if (System.currentTimeMillis() - lastCheck >= 1000) {
+        lastCheck = System.currentTimeMillis();
+        System.out.println("FPS: " + frames + " | UPS: " + updates);
+        frames = 0;
+        updates = 0;
       }
     }
   }
+
+  public void windowFocusLost(){
+    player.resetDirBooleans();
+  }
+}
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -1,6 +1,8 @@
 package main;
 
 import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 
 public class GameWindow {
   private JFrame jframe;
@@ -10,10 +12,19 @@ public class GameWindow {
 
     jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Shuts down when clicking Exit
     jframe.add(gamePanel); //add panel in window
-    //jframe.setLocationRelativeTo(null); //open window in the middle of your screen
     jframe.setResizable(false);
     jframe.pack();
     jframe.setLocationRelativeTo(null); //open window in the middle of your screen
     jframe.setVisible(true); //Make window visible
+    jframe.addWindowFocusListener(new WindowFocusListener() {
+      @Override
+      public void windowGainedFocus(WindowEvent e) {
+      }
+
+      @Override
+      public void windowLostFocus(WindowEvent e) {
+        gamePanel.getGame().windowFocusLost();
+      }
+    });
   }
 }
