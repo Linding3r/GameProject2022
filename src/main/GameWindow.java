e55032790984429851ab0c891928cb8ccfc2177f
Linding3r <thomasjuullindinger@yahoo.dk>
2022-04-23 11:27:09 +0200

    -Added player_sprite
    Now working moving sprite on screen controled by WASD

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -8,9 +8,11 @@ public class GameWindow {
   public GameWindow(GamePanel gamePanel){
     jframe = new JFrame();
 
-    jframe.setSize(1280,700);
     jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Shuts down when clicking Exit
     jframe.add(gamePanel); //add panel in window
+    //jframe.setLocationRelativeTo(null); //open window in the middle of your screen
+    jframe.setResizable(false);
+    jframe.pack();
     jframe.setLocationRelativeTo(null); //open window in the middle of your screen
     jframe.setVisible(true); //Make window visible
   }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,10 @@
 package main;
 
+
 public class Main {
 
     public static void main(String[] args) {
-    Game game = new Game();
-    game.run();
+        Game game = new Game();
+        game.run();
     }
 }
